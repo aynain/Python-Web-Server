dff09faf2b03a51aa4fde4ae9b5df0a972bf11f2
alina <alina.raza@yahoo.com>
2022-08-26 00:18:37 +0500

    2nd commit

---
+++ webFile.py
@@ -0,0 +1,31 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://ChideraCruelity.com</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>Here You are on web server.<br> Please Enter Your Details</p>", "utf-8"))
+        self.wfile.write(bytes("""<label>UserName:</label><input type="text"><br>
+        <label>Password:</label><input type="password"><br><input type="submit" value="Submit">""", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
